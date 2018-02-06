1c9341e3e068ea3a65f05d98a169d7fd5481b732
cwy007 <chanweiyan007@gmail.com>
2018-02-06 18:37:18 +0800

    create task and user model and related migration

---
+++ db/migrate/20180206103152_create_tasks.rb
@@ -0,0 +1,9 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.boolean :complete, default: false, null: false 
+    end
+  end
+end
